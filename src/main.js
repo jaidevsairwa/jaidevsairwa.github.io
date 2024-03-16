8f3f043eb08009c3081723ab50b2a7e0ad1cd3f5
Jay S <jaidevsairwa@gmail.com>
2024-03-16 23:26:31 +0530

    namaste speed change

--- src/main.js
+++ src/main.js
@@ -103,15 +103,15 @@ document.querySelector("h5").onmouseover = event => {
 async function init() {
     const node = document.querySelector("#type-text")
 
-    await sleep(1000)
+    await sleep(1500)
     node.innerText = '|'
 
     while (true) {
         await node.type('नमस्ते')
-        await sleep(2000)
+        await sleep(3000)
         await node.delete('नमस्ते')
         await node.type('Namaste')
-        await sleep(2000)
+        await sleep(3000)
         await node.delete('Namaste')
     }
 }
@@ -120,22 +120,17 @@ async function init() {
 const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
 class TypeAsync extends HTMLSpanElement {
-    get typeInterval() {
-        const randomMs = 100 * Math.random()
-        return randomMs < 50 ? 10 : randomMs
-    }
-
     async type(text) {
         for (let character of text) {
             this.innerText += character
-            await sleep(this.typeInterval)
+            await sleep(160)
         }
     }
 
     async delete(text) {
         for (let character of text) {
             this.innerText = this.innerText.slice(0, this.innerText.length - 1)
-            await sleep(this.typeInterval)
+            await sleep(160)
         }
     }
 }
